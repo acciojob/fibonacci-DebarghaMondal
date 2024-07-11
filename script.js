6ad02ef3f6b521d46c37076297b2def32effee5c
DebarghaMondal <166617838+DebarghaMondal@users.noreply.github.com>
2024-07-11 19:16:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function fibonacci(num) {
-// your code here
-	 return num < 1 ? 0
-        : num <= 2 ? 1
-        : fibonacci(num - 1) + fibonacci(num - 2)
+  if (num === 1) return 0;
+  else if (num === 2) return 1;
+  num -= 2;
+  var num1 = 0;
+  var num2 = 1;
+  var sum;
+  var i = 0;
+  for (i = 0; i < num; i++) {
+    sum = num1 + num2;
+    num1 = num2;
+    num2 = sum;
+  }
+  return num2;
 }
 
 module.exports = fibonacci;
